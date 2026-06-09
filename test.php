e994ad83a455e195616945449f45199407838636
Villacreces <luismateovillacrecesmurgueitio@gmail.com>
2026-06-09 12:52:33 -0500

    ProyectoV7

--- test.php
+++ test.php
@@ -1,20 +1,12 @@
 <?php
 
-echo "<h2>Extensiones PHP</h2>";
+echo "<pre>";
 
 echo "PDO: ";
 var_dump(extension_loaded('pdo'));
 
-echo "<br>";
-
 echo "PDO_MYSQL: ";
 var_dump(extension_loaded('pdo_mysql'));
 
-echo "<br>";
-
 echo "MYSQLI: ";
 var_dump(extension_loaded('mysqli'));
-
-echo "<hr>";
-
-phpinfo();
