fe02b3b282fb7d7fb0e66523465f2cdfce378bf0
Villacreces <luismateovillacrecesmurgueitio@gmail.com>
2026-06-09 14:01:42 -0500

    ProyectoV18

--- conection.php
+++ conection.php
@@ -1,4 +1,19 @@
+<?php
+echo "<pre>";
+echo "MYSQLHOST: ";
+var_dump(getenv("MYSQLHOST"));
 
+echo "MYSQLDATABASE: ";
+var_dump(getenv("MYSQLDATABASE"));
+
+echo "MYSQLUSER: ";
+var_dump(getenv("MYSQLUSER"));
+
+echo "MYSQLPORT: ";
+var_dump(getenv("MYSQLPORT"));
+
+exit;
+?>
 <?php
 
 try {
